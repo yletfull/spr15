52d100f11bbf3816797d9fc1950fea6c3f32b193
Andrey Korlyakov <one5640@yandex.ru>
2020-06-13 15:21:45 +0300

    fix eslint errors

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+/* eslint-disable import/no-dynamic-require */
 const express = require('express');
 const path = require('path');
-const routes = require(path.join(__dirname,'./routes/routes'));
+
+const routes = require(path.join(__dirname, './routes/routes'));
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const { error, timeLog } = require(path.join(__dirname, "/routes/helpers.js"));
+const { error, timeLog } = require(path.join(__dirname, '/routes/helpers.js'));
 
 app.use(express.static(`${__dirname}/public`));
 app.use('', timeLog);
 app.use('', routes);
 app.use('', error);
 
-const server = app.listen(PORT, () => {});
+app.listen(PORT, () => {});
